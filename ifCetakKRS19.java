f1664836dd1072cb662d5359762f1ab9fe88c9cb
nashyra24 <140595105+nashyra24@users.noreply.github.com>
2025-10-01 08:12:59 +0700

    Modifikasi Percobaan 1

--- ifCetakKRS19.java
+++ ifCetakKRS19.java
@@ -10,7 +10,12 @@ public class ifCetakKRS19 {
         if (uktLunas) {
             System.out.println("Pembayaran UKT terverifikasi");
             System.out.println("Silahkan cetak KRS dan minta tanda tangan DPA");
+        } else {
+            System.out.println("Registrasi ditolak. Silakan lunasi UKT terlebih dahulu");
         }
+
+        sc.close();
+        
 
     }
 }
